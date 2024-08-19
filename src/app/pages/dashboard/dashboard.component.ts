128dffac5e113b50cd138adc24e57e9430bd8166
sanket-angularminds <sanket@angularminds.in>
2024-08-19 16:44:19 +0530

    code cleanup

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,42 +1,8 @@
 import { Component } from '@angular/core';
-import { Nft } from 'src/app/shared/models/nft.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-  nft: Array<Nft>;
-
-  constructor() {
-    this.nft = [
-      {
-        id: 34356771,
-        title: 'Girls of the Cartoon Universe',
-        creator: 'Jhon Doe',
-        instant_price: 4.2,
-        price: 187.47,
-        ending_in: '06h 52m 47s',
-        last_bid: 0.12,
-        image: './assets/images/daemon.png',
-        avatar: './assets/avatars/avt-01.jpg',
-      },
-      {
-        id: 34356772,
-        title: 'Pupaks',
-        price: 548.79,
-        last_bid: 0.35,
-        image: './assets/images/img-02.jpg',
-      },
-      {
-        id: 34356773,
-        title: 'Seeing Green collection',
-        price: 234.88,
-        last_bid: 0.15,
-        image: './assets/images/img-03.jpg',
-      },
-    ];
-  }
-
-}
+export class DashboardComponent {}
